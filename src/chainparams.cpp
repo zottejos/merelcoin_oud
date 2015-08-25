4db9463ea98919f3cdc733fcbf8e7a71546cb3e2
zottejos <zottejos@gmail.com>
2015-08-25 11:59:30 +0200

    tijd tussen blocks verkort naar 30s

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -109,7 +109,7 @@ public:
         nToCheckBlockUpgradeMajority = 1000;
         nMinerThreads = 0;
         nTargetTimespan = 3.5 * 24 * 60 * 60; // 3.5 days
-        nTargetSpacing = 2.5 * 60; // 2.5 minutes
+        nTargetSpacing = 1 * 30; // 2.5 minutes
 
         /**
          * Build the genesis block. Note that the output of the genesis coinbase cannot
