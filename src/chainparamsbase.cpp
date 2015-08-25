662e89bdddb285be350f1916c6603098f2ce6d08
zottejos <zottejos@gmail.com>
2015-08-25 09:15:36 +0200

    poorten aangepast naar 60808 160808 20683 120683

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 9332;
+        nRPCPort = 20683;
     }
 };
 static CBaseMainParams mainParams;
@@ -36,7 +36,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 19332;
+        nRPCPort = 120683;
         strDataDir = "testnet3";
     }
 };
